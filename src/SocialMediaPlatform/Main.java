50e37f908d86b6069d1bf4d9f7291315e1ee87ab
ahmdhqnn <ahmadhaqinn@gmail.com>
2024-11-28 20:10:50 +0700

    first commit

---
+++ src/SocialMediaPlatform/Main.java
@@ -0,0 +1,9 @@
+package SocialMediaPlatform;
+
+import View.Welcome;
+
+public class Main {
+	public static void main(String []args) {
+		new Welcome();
+	}
+}
---
+++ src/View/JFrame.java
@@ -0,0 +1,12 @@
+package View;
+
+public class JFrame extends javax.swing.JFrame {
+
+	public JFrame() {
+		super("IGBeta");
+		getContentPane().setBackground(GUIConstants.background);
+		setSize(900,625);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+	}
+} 
